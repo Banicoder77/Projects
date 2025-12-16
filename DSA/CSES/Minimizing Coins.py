3019dd04978e5fe84800dfff9b76f160c82ec0f2
Banicoder77 <cs1252118@iitd.ac.in>
2025-12-17 00:36:50 +0530

    more cleanup

--- DSA/CSES/Minimizing Coins.py
+++ DSA/CSES/Minimizing Coins.py
@@ -1,13 +1,10 @@
 from array import array
 n, x = map(int, input().split())
 c = list(map(int, input().split()))
- 
 INF = int(1e9)
 dp = array('i', [INF] * (x + 1))
 dp[0] = 0
- 
 for coin in c:
     for i in range(coin, x + 1):
-        dp[i] = min(dp[i], dp[i - coin] + 1)
- 
+        dp[i] = min(dp[i], dp[i - coin] + 1) 
 print(dp[x] if dp[x] != INF else -1)
