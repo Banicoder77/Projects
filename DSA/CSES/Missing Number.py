5532f2b6a0f80083ab57b28f152964ed09b6b9a6
Bani Brata Majee <cs1252118@iitd.ac.in>
2026-02-17 18:20:29 +0530

    Delete DSA directory

--- DSA/CSES/Missing Number.py
+++
@@ -1,9 +0,0 @@
-x=int(input())
-j=list(map(int,input().split()))
-l=[False]*x
-for i in j:
-    l[i-1]=True
-for c,d in enumerate(l):
-    if d==False:
-        print(c+1)
-        break
--- DSA/CSES/Weird Algorithm.py
+++
@@ -1,9 +0,0 @@
-x=int(input())
-j=[str(x)]
-while x!=1:
-    if x%2==0:
-        x=x//2
-    else:
-        x=3*x+1
-    j.append(str(x))
-print(" ".join(j))
